513026761ec6f4f982b7fff3536119b01d0c8957
MandiriTraining <111072208+MandiriTraining@users.noreply.github.com>
2022-08-15 09:37:01 +0700

    benerin

--- landing_page/views.py
+++ landing_page/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(requst):
-    return HttpResponse ("cek django")
+    #return HttpResponse ("cek django")
     return render(request, 'landing_page/index.html')
 
 # Create your views here.
